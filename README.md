909296d273a7edd3f41df5c7ed858693ffefea02
CharlesSchimmel <charlesschimmel@gmail.com>
2020-04-04 19:04:31 -0500

    one line per title

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # streaming-test
 
+This is an exercise in using and understanding the Reddit API as well as playing
+around with streams.
+
 The Reddit library "PRAW" has a really nice way of creating infinite streams of
 new posts for a given subreddit that can then be acted on. This little project
 is an attempt to recreate that functionality in Haskell with the `Streaming`
--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,12 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE NamedFieldPuns #-}
+
 module Main where
 
 import Data.Either
+import Data.Text as T
 import Streaming.Prelude as S
 
 import Lib
@@ -8,7 +14,10 @@ import Types
 
 main :: IO ()
 main =
-  S.print . S.take 10 . S.delay 30 . S.filter isRight . S.map postTitles $
+  S.mapM_ linePerTitle .
+  S.take 10 . S.delay 30 . S.filter isRight . S.map postTitles $
   newSubStream "earthporn"
   where
-    postTitles = fmap ((fmap title) . children . listingData)
+    postTitles = fmap ((fmap $ T.unpack . title) . children . listingData)
+    linePerTitle (Right titles) = Prelude.mapM_ putStrLn titles
+    linePerTitle _ = pure ()
